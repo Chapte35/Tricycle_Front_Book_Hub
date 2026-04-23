1ad9505be1522f1dfac309bc3a29737638adacfd
audrey <audrey.pehuet@gmail.com>
2026-04-23 11:56:06 +0200

    US-AUTH-03 | Profil init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register';
 import { Home } from './pages/home/home';
 import {AuthComponent} from './pages/auth/auth';
+import { ProfileComponent } from './components/profile/profile';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'login', component: AuthComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'profile', component: ProfileComponent },
   ]
