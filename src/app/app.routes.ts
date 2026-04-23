bc0fac137ceee55b7d58dbf0a8904e851f683f1c
audrey <audrey.pehuet@gmail.com>
2026-04-23 11:56:06 +0200

    US-AUTH-03 | Profil init
    # Conflicts:
    #	src/app/app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Myloans } from './pages/my-loans/my-loans';
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register';
 import { Home } from './pages/home/home';
+import {AuthComponent} from './pages/auth/auth';
+import { ProfileComponent } from './components/profile/profile';
 import { BookDetail } from './pages/book-detail/book-detail';
 import { LoginComponent } from './pages/login/login';
 
@@ -9,6 +11,7 @@ export const routes: Routes = [
     { path: '', component: Home },
     { path: 'login',    component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'profile', component: ProfileComponent },
     { path: 'books/:id', component: BookDetail },
     { path: 'myloans', component: Myloans },
   ]
--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProfileComponent } from "../../components/profile/profile";
+import { ProfileService } from '../../services/profile-service';
+import { User } from '../../interfaces/user';
 
 @Component({
-  selector: 'app-profile',
-  imports: [],
-  templateUrl: './profile.html',
+  selector: 'app-profile-page',
+  standalone: true,
+  imports: [ProfileComponent],
+  templateUrl: './profile.html', 
   styleUrl: './profile.css',
 })
-export class Profile {
+export class ProfilePage implements OnInit {
 
+  private profileService = inject(ProfileService); 
+  user: User | null = null;
+  private userId: number = JSON.parse(localStorage.getItem('user') || '{}').id;
+
+  ngOnInit() {
+    this.profileService.getUser(this.userId).subscribe(user => {
+      this.user = user;
+    });
+  }
+
+  onSave(updatedUser: Partial<User>) {
+    this.profileService.updateProfile(this.userId, updatedUser).subscribe(user => {
+      this.user = user;
+      console.log('Profil mis à jour', user);
+    });
+  }
 }
