159dd0e5264a27b477ca884ca6c0d5f9d7c68db2
audrey <audrey.pehuet@gmail.com>
2026-04-23 16:48:12 +0200

    Condition connection/incription

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Myloans } from './pages/my-loans/my-loans';
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register';
 import { Home } from './pages/home/home';
-import {AuthComponent} from './pages/auth/auth';
 import { ProfileComponent } from './components/profile/profile';
 import { BookDetail } from './pages/book-detail/book-detail';
 import { LoginComponent } from './pages/login/login';
